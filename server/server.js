307fe4a0ea72be7cf5c3fcae308178285b7101c8
Radoslava Gettova <radka.gettova@thinkcreatix.com>
2018-02-17 19:05:26 +0100

    Setup for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {Todo} = require("./models/todo");
 const {User} = require("./models/user");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -33,8 +34,8 @@ app.get("/todos", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Started on port 3000");
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
